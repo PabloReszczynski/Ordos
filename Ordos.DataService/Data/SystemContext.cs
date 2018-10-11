0fe622a94cd92f0fc6482e91c8bd30100dfd81f3
Pablo Reszczynski <pablore@me.com>
2018-10-11 17:31:53 -0300

    Take out database connection string from code and putting it on an app.config file, which is not part of the repo. Addressing, but not completing #40.

--- Ordos.DataService/Data/SystemContext.cs
+++ Ordos.DataService/Data/SystemContext.cs
@@ -1,14 +1,14 @@
 ﻿using Ordos.Core.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 
 namespace Ordos.DataService.Data
 {
     public class SystemContext : DbContext
     {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-        public static string SQLExpressConnectionString =>
-            "Server=(localdb)\\mssqllocaldb;Database=Ordos;ConnectRetryCount=0;Trusted_Connection=True;MultipleActiveResultSets=true";
 
+        public static string SQLExpressConnectionString =>
+              ConfigurationManager.AppSettings["server"].ToString();
         public SystemContext()
         {
         }
